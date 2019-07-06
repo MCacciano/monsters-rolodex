ef8681a1f813baaec6984d4d09f12c32188c0dce
Michael Cacciano <michael.cacciano@gmail.com>
2019-07-06 10:21:36 -0400

    Updates

---
+++ precache-manifest.1870846c341c219548a91cbc58a8b79a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d21d1c471c12b889d9c7103c98fc29b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7491f7102c8631475528",
+    "url": "/monsters-rolodex/static/css/main.0df0de3e.chunk.css"
+  },
+  {
+    "revision": "1bb63ee44dd56da8a756",
+    "url": "/monsters-rolodex/static/js/2.e0582f57.chunk.js"
+  },
+  {
+    "revision": "7491f7102c8631475528",
+    "url": "/monsters-rolodex/static/js/main.551e730f.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
